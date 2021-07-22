eb5ba92993432ad780f6d4fea04ca31d18a98ecf
Chengsn <csn1419125124@gmail.com>
2021-07-22 23:42:25 +0800

    新增

--- day01/boot.asm
+++ day01/boot.asm
@@ -7,12 +7,12 @@
 DispStr:
     mov     ax, BootMessage
     mov     bp, ax              ; ES:BP = 串地址
-    mov     cx, 16              ; CX = 串长度
+    mov     cx, 20              ; CX = 串长度
     mov     ax, 01301h          ; AH = 13, AL = 01301h
-    mov     bx, 000ch           ; 页号为0(BH = 0), 黑底红字(BL = 0Ch) 
+    mov     bx, 00f8h           ; 页号为0(BH = 0), 黑底红字(BL = 0Ch) 
     mov     dl, 0
     int     10h                 ; 10h 号中断
     ret
-BootMessage:    db      "ello, icgOS World!"
+BootMessage:    db      "Hello, icgOS World!"
 times   510 - ($ - $$)    db     0      ; 填充剩下的空间，使生成的二进制代码恰好为512字节
 dw      0xaa55                          ; 结束标志
